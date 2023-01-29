a0434b69759c4261acdca23095fb23177f3fbdd7
shapu <shashank.pulagam@gmail.com>
2023-01-29 23:58:22 +0530

    Fixed issues in data.js

--- src/data.js
+++ src/data.js
@@ -64,7 +64,7 @@ export const tours = [
     date: "august 26th, 2020",
     title: "Tibet Adventure",
     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque"+
-              "vitae tempore voluptatum maxime reprehenderit eum quod"
+              "vitae tempore voluptatum maxime reprehenderit eum quod"+
               "exercitationem fugit, qui corporis.",
     location: "China",
     duration: 6,
